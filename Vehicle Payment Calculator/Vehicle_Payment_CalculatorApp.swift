e18a00818833e74e4a388af76d34f621ebb9c478
Logan Nelsen <loganjnelsen@gmail.com>
2025-03-28 17:07:38 -0500

    Initial Commit

---
+++ Vehicle Payment Calculator/Vehicle_Payment_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vehicle_Payment_CalculatorApp.swift
+//  Vehicle Payment Calculator
+//
+//  Created by Logan Nelsen on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Vehicle_Payment_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vehicle Payment CalculatorTests/Vehicle_Payment_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Vehicle_Payment_CalculatorTests.swift
+//  Vehicle Payment CalculatorTests
+//
+//  Created by Logan Nelsen on 3/28/25.
+//
+
+import Testing
+@testable import Vehicle_Payment_Calculator
+
+struct Vehicle_Payment_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
